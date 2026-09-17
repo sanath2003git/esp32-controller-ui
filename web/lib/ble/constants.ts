3516ad22e84d24eed56c492953a2edc13b5f9550
Abhaj Khan <abhajkhan02@gmail.com>
2026-09-17 15:45:11 +0530

    BLE Device name changed to ELXIE

--- web/lib/ble/constants.ts
+++ web/lib/ble/constants.ts
@@ -7,4 +7,4 @@ export const BLE_RX_CHARACTERISTIC_UUID =
 export const BLE_TX_CHARACTERISTIC_UUID =
   "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 
-export const BLE_DEVICE_NAME = "Elxie-CQ";
+export const BLE_DEVICE_NAME = "ELXIE";
